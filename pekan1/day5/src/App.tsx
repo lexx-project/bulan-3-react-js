6af5f6fe831ee4967b6626dd9828c4e6dc6917ed
lexx-project <passlexyganteng@gmail.com>
2025-10-06 20:01:38 +0700

    feat: add task for day 5 in week 1 ReactJs

--- pekan1/day5/src/App.tsx
+++ pekan1/day5/src/App.tsx
@@ -1,15 +1,14 @@
-// import Counter from "./components/counter";
-// import FormData from "./components/FormData";
-// import TodoList from "./components/TodoList";
-
+import Counter from "./components/counter";
+import FormData from "./components/FormData";
+import TodoList from "./components/TodoList";
 import UserProfile from "./components/UserProfile";
 
 export default function App() {
   return (
     <>
-      {/* <Counter /> */}
-      {/* <FormData /> */}
-      {/* <TodoList /> */}
+      <Counter />
+      <FormData />
+      <TodoList />
       <UserProfile />
     </>
   );
